d705fde26832ed1c82747d500ab7ad122a8a175b
bredele <olivier.wietrich@gmail.com>
2014-09-23 20:04:39 -0600

    start working on example

--- example/motion.js
+++ example/motion.js
@@ -1,1 +1,46 @@
+
+/**
+ * Example dependency.
+ */
+
 var drag = require('..');
+
+
+/**
+ * Kick button and
+ * listener.
+ */
+
+var btn = document.querySelector('.kick');
+btn.addEventListener('click', kick);
+
+
+/**
+ * Default ball
+ * position.
+ */
+
+var x = 240;
+var y = 200;
+
+
+/**
+ * [kick description]
+ * @return {[type]} [description]
+ */
+
+function kick() {
+
+}
+
+/**
+ * Set ball position.
+ * 
+ * @param  {Number} x
+ * @param  {Number} y
+ * @api private
+ */
+
+function position(x, y) {
+	btn.setAttribute('style', 'top:; left:;');
+}
